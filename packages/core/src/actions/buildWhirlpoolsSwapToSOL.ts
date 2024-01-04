fa3e71fa663aa8f8cc312f62a6eb3fdc8aacdd47
praskoson <prasinar@gmail.com>
2024-01-04 15:09:51 +0100

    wip: transition to using jupiter API instead of orca for swapping

--- packages/core/src/actions/buildWhirlpoolsSwapToSOL.ts
+++ packages/core/src/actions/buildWhirlpoolsSwapToSOL.ts
@@ -23,6 +23,23 @@ export type FeeOptions = {
     burnFeeBp: number;
 };
 
+export type QuoteResponse = {
+    inputMint: string;
+    inAmount: string;
+    outputMint: string;
+    outAmount: string;
+    otherAmountThreshold: string;
+    swapMode: 'ExactIn' | 'ExactOut';
+    slippageBps: number;
+    platformFee?: {
+        amount: string;
+        feeBps: number;
+    };
+    priceImpactPct: string;
+    contextSlot?: number;
+    timeTaken?: number;
+};
+
 /**
  * Builds an unsigned transaction that performs a swap to SOL and optionally sends a token fee to Octane
  *
@@ -48,7 +65,7 @@ export async function buildWhirlpoolsSwapToSOL(
     cache: Cache,
     sameMintTimeout = 3000,
     feeOptions?: FeeOptions
-): Promise<{ transaction: Transaction; quote: SwapQuote; messageToken: string }> {
+): Promise<{ transaction: Transaction; quote: QuoteResponse; messageToken: string }> {
     // Connection's genesis hash is cached to prevent an extra RPC query to the node on each call.
     const genesisHashKey = `genesis/${connection.rpcEndpoint}`;
     let genesisHash = await cache.get<string>(genesisHashKey);
@@ -84,66 +101,126 @@ export async function buildWhirlpoolsSwapToSOL(
     const transferFee = feeOptions?.transferFeeBp ? amount.muln(feeOptions.transferFeeBp).divn(10000) : new BN(0);
     const swapAmount = amount.sub(burnFee).sub(transferFee);
 
-    const context = whirlpools.getWhirlpoolsContext(connection);
-    const [mintA, mintB] = whirlpools.getABMints(sourceMint, NATIVE_MINT);
-    const [whirlpool, quote] = await whirlpools.getPoolAndQuote(
-        context,
-        mintA,
-        mintB,
-        sourceMint,
-        swapAmount,
-        slippingTolerance
-    );
+    // Swapping SOL to USDC with input 0.1 SOL and 0.5% slippage
+    const outputMint = 'So11111111111111111111111111111111111111112';
+    const params = new URLSearchParams();
+    params.append('inputMint', sourceMint.toString());
+    params.append('outputMint', outputMint);
+    params.append('amount', swapAmount.toString()); // Convert number to string
+    params.append('asLegacyTransaction', 'true');
+    // params.append('slippageBps', slippageBps.toString());
 
-    const swapInstructions = await whirlpools.getSwapInstructions(
-        feePayer.publicKey,
-        user,
-        context,
-        whirlpool,
-        quote,
-        await getMinimumBalanceForRentExemptAccount(connection)
-    );
+    const url = new URL('https://quote-api.jup.ag/v6/quote');
+    url.search = params.toString();
 
-    let feeBurnInstruction: TransactionInstruction | undefined;
-    let feeTransferInstruction: TransactionInstruction | undefined;
-    if (feeOptions !== undefined && burnFee.gtn(0)) {
-        feeBurnInstruction = createBurnInstruction(
-            feeOptions.sourceAccount,
-            sourceMint,
-            user,
-            BigInt(burnFee.toString())
-        );
+    const quoteResponse = await (await fetch(url)).json();
+    const instructions = await (
+        await fetch('https://quote-api.jup.ag/v6/swap-instructions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                quoteResponse,
+                userPublicKey: user.toString(),
+                wrapAndUnwrapSol: true,
+                asLegacyTransaction: true,
+            }),
+        })
+    ).json();
+
+    if (instructions.error) {
+        throw new Error('Failed to get swap instructions: ' + instructions.error);
     }
-    if (feeOptions !== undefined && transferFee.gtn(0)) {
-        feeTransferInstruction = createTransferInstruction(
-            feeOptions.sourceAccount,
-            feeOptions.destinationAccount,
-            user,
-            BigInt(transferFee.toString())
-        );
-    }
+    const {
+        computeBudgetInstructions, // The necessary instructions to setup the compute budget.
+        setupInstructions, // Setup missing ATA for the users.
+        swapInstruction: swapInstructionPayload, // The actual swap instruction.
+        cleanupInstruction, // Unwrap the SOL if `wrapAndUnwrapSol = true`.
+    } = instructions;
 
-    const instructions: TransactionInstruction[] = [...swapInstructions];
-    if (feeBurnInstruction) instructions.unshift(feeBurnInstruction);
-    if (feeTransferInstruction) instructions.unshift(feeTransferInstruction);
+    const deserializeInstruction = (instruction: any) => {
+        try {
+            return new TransactionInstruction({
+                programId: new PublicKey(instruction.programId),
+                keys: instruction.accounts.map((key: any) => ({
+                    pubkey: new PublicKey(key.pubkey),
+                    isSigner: key.isSigner,
+                    isWritable: key.isWritable,
+                })),
+                data: Buffer.from(instruction.data, 'base64'),
+            });
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    };
+
+    // const blockhash = (await connection.getLatestBlockhash()).blockhash;
+    // const;
+
+    // const messageV0 = new TransactionMessage({
+    //     payerKey: payerPublicKey,
+    //     recentBlockhash: blockhash,
+    //     instructions: [
+    //         // uncomment if needed: ...setupInstructions.map(deserializeInstruction),
+    //         deserializeInstruction(swapInstructionPayload),
+    //         // uncomment if needed: deserializeInstruction(cleanupInstruction),
+    //     ],
+    // }).compileToV0Message(addressLookupTableAccounts);
+    // const transaction = new VersionedTransaction(messageV0);
+
+    // let feeBurnInstruction: TransactionInstruction | undefined;
+    // let feeTransferInstruction: TransactionInstruction | undefined;
+    // if (feeOptions !== undefined && burnFee.gtn(0)) {
+    //     feeBurnInstruction = createBurnInstruction(
+    //         feeOptions.sourceAccount,
+    //         sourceMint,
+    //         user,
+    //         BigInt(burnFee.toString())
+    //     );
+    // }
+    // if (feeOptions !== undefined && transferFee.gtn(0)) {
+    //     feeTransferInstruction = createTransferInstruction(
+    //         feeOptions.sourceAccount,
+    //         feeOptions.destinationAccount,
+    //         user,
+    //         BigInt(transferFee.toString())
+    //     );
+    // }
+
+    // const instructions: TransactionInstruction[] = [
+    //     ...computeBudgetInstructions,
+    //     ...setupInstructions,
+    //     swapInstruction,
+    //     cleanupInstruction,
+    // ];
+
+    // if (feeBurnInstruction) instructions.unshift(feeBurnInstruction);
+    // if (feeTransferInstruction) instructions.unshift(feeTransferInstruction);
 
     const transaction = new Transaction({
         feePayer: feePayer.publicKey,
         ...(await connection.getLatestBlockhash()),
     })
-        .add(...instructions)
-        .add(...getPriorityFeeInstructions());
+        .add(...computeBudgetInstructions.map(deserializeInstruction))
+        .add(...setupInstructions.map(deserializeInstruction))
+        .add(deserializeInstruction(swapInstructionPayload))
+        .add(deserializeInstruction(cleanupInstruction));
 
     await simulateRawTransaction(connection, transaction.serialize({ verifySignatures: false }));
 
-    const messageToken = new MessageToken(
-        whirlpools.MESSAGE_TOKEN_KEY,
-        transaction.compileMessage(),
-        feePayer
-    ).compile();
+    let messageToken: any;
+    try {
+        // const compiled
+        messageToken = new MessageToken(whirlpools.MESSAGE_TOKEN_KEY, transaction.compileMessage(), feePayer).compile();
+    } catch (e) {
+        console.log('Error creating token');
+        throw e;
+    }
 
     // set last signature for mint and user
     await cache.set<number>(key, Date.now());
 
-    return { transaction, quote, messageToken };
+    return { transaction, quote: quoteResponse, messageToken };
 }
